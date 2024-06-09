e64822df90a8c4f0240242ca62e789b139e49364
olivier <mupenziolivier@gmail.com>
2024-06-09 12:10:58 +0300

    few fixes

--- scripts.js
+++ scripts.js
@@ -37,3 +37,27 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// Example usage:
+console.log(reverseString("hello")); // Output: "olleh"
+function fizzBuzz() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+// Example usage:
+fizzBuzz();
+
